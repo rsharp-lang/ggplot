7b2a3372094c3b348862c79ff5c62c0fd008e7af
xieguigang <xie.guigang@gcmodeller.org>
2021-08-26 01:47:25 +0800

    add api

--- src/ggplot2.vb
+++ src/ggplot2.vb
@@ -1,3 +1,20 @@
-﻿Public Class ggplot2
+﻿Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
+Imports SMRUCC.Rsharp.Runtime.Interop
+
+<Package("ggplot2")>
+Public Class ggplot2
+
+    <ExportAPI("ggplot")>
+    Public Function ggplot(<RRawVectorArgument>
+                           Optional data As Object = Nothing,
+                           Optional mapping As Object = "~aes()",
+                           <RListObjectArgument>
+                           Optional args As list = Nothing,
+                           Optional environment As Environment = Nothing)
+
+    End Function
 
 End Class
