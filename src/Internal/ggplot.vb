d4ef676f42cabc5e79fe34550e36a6aaa8c68b90
xieguigang <xie.guigang@gcmodeller.org>
2021-10-03 23:52:26 +0800

    handle create image output

--- src/Internal/ggplot.vb
+++ src/Internal/ggplot.vb
@@ -1,9 +1,15 @@
-﻿Imports SMRUCC.Rsharp.Runtime.Internal.Object
+﻿Imports System.Drawing.Imaging
+Imports System.IO
+Imports Microsoft.VisualBasic.Imaging.BitmapImage
+Imports SMRUCC.Rsharp.Runtime.Internal.Object
 
-Public Class ggplot
+Public Class ggplot : Implements SaveGdiBitmap
 
     Public Property data As Object
     Public Property layers As New List(Of ggplotLayer)
     Public Property args As list
 
+    Public Function Save(stream As Stream, format As ImageFormat) As Boolean Implements SaveGdiBitmap.Save
+        Throw New NotImplementedException()
+    End Function
 End Class
