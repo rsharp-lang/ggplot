574782ce626573373fd9ad0de07c936c3fcd03f8
xieguigang <xie.guigang@gcmodeller.org>
2021-10-06 00:40:27 +0800

    split source file module

---
+++ src/Internal/ggplotBase.vb
@@ -0,0 +1,8 @@
+﻿''' <summary>
+''' 只绘制出一个基本的坐标轴
+''' </summary>
+Public Class ggplotBase
+
+    Public Property reader As ggplotReader
+
+End Class
--- src/Internal/layers/ggplotLayer.vb
+++ src/Internal/layers/ggplotLayer.vb
@@ -20,27 +20,3 @@ Public MustInherit Class ggplotLayer
     )
 
 End Class
-
-''' <summary>
-''' 只绘制出一个基本的坐标轴
-''' </summary>
-Public Class ggplotBase
-
-    Public Property reader As ggplotReader
-
-End Class
-
-Public Class ggplotLine : Inherits ggplotLayer
-
-    Public Overrides Sub Plot(g As IGraphics,
-                              canvas As GraphicsRegion,
-                              baseData As ggplotData,
-                              x() As Double,
-                              y() As Double,
-                              scale As DataScaler,
-                              ggplot As ggplot,
-                              theme As Theme)
-
-        Throw New NotImplementedException()
-    End Sub
-End Class
