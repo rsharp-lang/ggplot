9c96b3cd4d2f14f7ff030c79ce48d37f00d14bba
xieguigang <xie.guigang@gcmodeller.org>
2021-10-08 21:44:06 +0800

    tweaks of the padding styles

--- test/volcano.R
+++ test/volcano.R
@@ -8,7 +8,7 @@ print("peeks of the raw data:");
 print(head(volcano));
 
 bitmap(file = `${@dir}/volcano.png`, size = [3000, 3600]) {
-	ggplot(volcano, aes(x = "log2FC", y = "p.value"), padding = "padding:150px 100px 200px 250px;")
+	ggplot(volcano, aes(x = "log2FC", y = "p.value"), padding = "padding:350px 100px 250px 350px;")
 	   + geom_point(color = "black", shape = "Circle", size = 21)
        + labs(x = "log2(FoldChange)", y = "-log10(P-value)")
        + ggtitle("Volcano Plot")
