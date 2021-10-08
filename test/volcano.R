3bf382516e3c878dfdc7269c19e9e20269b78f63
xieguigang <xie.guigang@gcmodeller.org>
2021-10-08 18:46:02 +0800

    black

--- test/volcano.R
+++ test/volcano.R
@@ -2,15 +2,13 @@ require(ggplot);
 
 const volcano = read.csv(`${@dir}/log2FC.csv`);
 
-volcano[, "class"]   = ifelse(as.numeric(volcano[, "p.value"]) < 0.05, "sig", "no-sig");
-volcano[, "class"]   = ifelse(abs(volcano[, "log2FC"]) < 1, "no-sig", volcano[, "class"]);
 volcano[, "p.value"] = -log10(volcano[, "p.value"]);
 
 print("peeks of the raw data:");
 print(head(volcano));
 
 bitmap(file = `${@dir}/volcano.png`, size = [3000, 3600]) {
-	ggplot(volcano, aes(x = "log2FC", y = "p.value", color = "class"), padding = "padding:150px 100px 200px 250px;")
+	ggplot(volcano, aes(x = "log2FC", y = "p.value"), padding = "padding:150px 100px 200px 250px;")
 	   + geom_point(color = "black", shape = "Circle", size = 21)
        + labs(x = "log2(FoldChange)", y = "-log10(P-value)")
        + ggtitle("Volcano Plot")
