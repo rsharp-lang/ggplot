dddcc1f98a3197fad86082a7abaf06cd3afa0ceb
xieguigang <xie.guigang@gcmodeller.org>
2021-10-08 22:59:47 +0800

    evaluate factors

--- test/volcano.R
+++ test/volcano.R
@@ -2,14 +2,17 @@ require(ggplot);
 
 const volcano = read.csv(`${@dir}/log2FC.csv`);
 
-print("peeks of the raw data:");
-print(head(volcano));
-
-volcano[, "factor"]  = ifelse(volcano[, "log2FC"] > 1, "Up", "Down");
+volcano[, "factor"]  = ifelse(volcano[, "log2FC"] >  log2(1.25), "Up", "Not Sig");
+volcano[, "factor"]  = ifelse(volcano[, "log2FC"] < -log2(1.25), "Down", volcano[, "factor"]);
 volcano[, "factor"]  = ifelse(volcano[, "p.value"] < 0.05, volcano[, "factor"], "Not Sig");
 volcano[, "p.value"] = -log10(volcano[, "p.value"]);
 
-
+print("peeks of the raw data:");
+print(head(volcano));
+print("count of the factors:");
+print(`Up:      ${sum("Up" == volcano[, "factor"])}`);
+print(`Not Sig: ${sum("Not Sig" == volcano[, "factor"])}`);
+print(`Down:    ${sum("Down" == volcano[, "factor"])}`);
 
 bitmap(file = `${@dir}/volcano.png`, size = [3000, 3600]) {
 	ggplot(volcano, aes(x = "log2FC", y = "p.value"), padding = "padding:350px 100px 250px 350px;")
