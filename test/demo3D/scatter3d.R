94967c73917ac42098c58088aee2b527c5482b67
xieguigang <xie.guigang@gcmodeller.org>
2021-11-06 15:56:22 +0800

    plot 3d test success

--- test/demo3D/scatter3d.R
+++ test/demo3D/scatter3d.R
@@ -2,9 +2,9 @@ require(ggplot);
 
 bitmap(file = `${@dir}/UMAP3d.png`, size = [3000, 3000]) {
     # create ggplot layers and tweaks via ggplot style options
-	ggplot(read.csv(`${@dir}/UMAP3D.csv`), aes(x = "X", y = "Y", z = "Z"), padding = "padding:250px 500px 250px 300px;")
-	+ geom_point(color = "black", shape = "circle", size = 25)
-	+ view_camera(angle = [30,65,125])
+	ggplot(read.csv(`${@dir}/UMAP3D.csv`, row.names = 1), aes(x = "X", y = "Y", z = "Z"), padding = "padding:50px 50px 50px 50px;")
+	+ geom_point(aes(color = "class"), color = "Set1", shape = "circle", size = 25)
+	+ view_camera(angle = [32,65,125], fov = 100000)
 	;
 
 }
--- test/demo3D/umap3D.R
+++ test/demo3D/umap3D.R
@@ -4,7 +4,7 @@ options(strict = FALSE);
 
 const filename as string = "E:\GCModeller\src\R-sharp\Library\demo\machineLearning\umap\MNIST-LabelledVectorArray-60000x100.msgpack";
 const MNIST_LabelledVectorArray = filename
-|> read.mnist.labelledvector(takes = 50000)
+|> read.mnist.labelledvector(takes = 5000)
 ;
 const tags as string = rownames(MNIST_LabelledVectorArray);
 
@@ -12,7 +12,7 @@ rownames(MNIST_LabelledVectorArray) = `X${1:nrow(MNIST_LabelledVectorArray)}`;
 
 const manifold = umap(MNIST_LabelledVectorArray,
 	dimension         = 3, 
-	numberOfNeighbors = 60,
+	numberOfNeighbors = 15,
 	localConnectivity = 1,
 	KnnIter           = 64,
 	bandwidth         = 1,
@@ -22,7 +22,7 @@ const manifold = umap(MNIST_LabelledVectorArray,
 ;
 
 manifold$umap
-|> as.data.frame
+|> as.data.frame(dimension = ["X", "Y", "Z"])
 |> write.csv( 
 	file      = `${@dir}/UMAP3D.csv`, 
 	row.names = tags
