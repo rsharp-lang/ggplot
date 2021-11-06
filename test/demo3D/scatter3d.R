b11a0b3ff038dadcfc49ff84237b2ba9634cc4c5
xieguigang <xie.guigang@gcmodeller.org>
2021-11-06 16:01:54 +0800

    tweaks of the color schema

--- test/demo3D/scatter3d.R
+++ test/demo3D/scatter3d.R
@@ -1,9 +1,9 @@
 require(ggplot);
 
-bitmap(file = `${@dir}/UMAP3d.png`, size = [3000, 3000]) {
+bitmap(file = `${@dir}/UMAP3d.png`, size = [2400, 2000]) {
     # create ggplot layers and tweaks via ggplot style options
 	ggplot(read.csv(`${@dir}/UMAP3D.csv`, row.names = 1), aes(x = "X", y = "Y", z = "Z"), padding = "padding:50px 50px 50px 50px;")
-	+ geom_point(aes(color = "class"), color = "Set1", shape = "circle", size = 25)
+	+ geom_point(aes(color = "class"), color = "Console", shape = "circle", size = 25)
 	+ view_camera(angle = [32,65,125], fov = 100000)
 	;
 
