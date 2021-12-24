d837299e96e3ec4843b636b9e93594a60032abeb
xieguigang <xie.guigang@gcmodeller.org>
2021-12-24 19:34:06 +0800

    Create lineTest.R

---
+++ test/lineTest.R
@@ -0,0 +1,10 @@
+require(ggplot);
+
+x = -5:5 step 0.1;
+y = sin(x);
+
+bitmap(file = `${@dir}/line_sin.png`, width = 2100, height = 1600) {
+    ggplot(data.frame(x = x, y =y)) 
+    + geom_line()
+    ;
+}
