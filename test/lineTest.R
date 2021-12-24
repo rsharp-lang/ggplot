ea9fcd35dfa5757e8216d1db118b07194a96a7ce
xieguigang <xie.guigang@gcmodeller.org>
2021-12-24 21:06:21 +0800

    tweaks of the plot styles

--- test/lineTest.R
+++ test/lineTest.R
@@ -4,7 +4,7 @@ x = seq(-5,5, by = 0.2);
 y = sin(x);
 
 bitmap(file = `${@dir}/line_sin.png`, width = 2400, height = 1600) {
-    ggplot(data.frame(x = x, y =y), aes(x = "x", y = "y"), padding = "padding: 200px 450px 200px 200px;") 
+    ggplot(data.frame(x = x, y =y), aes(x = "x", y = "y"), padding = "padding: 200px 500px 200px 200px;") 
     + geom_line(width = 8, show.legend = TRUE, color = "Jet")
     ;
 }
