b6540b7fd559a209c03ddc3f229c70f0ae580671
guigang xie <gg.xie@bionovogene.com>
2021-12-31 12:19:25 +0800

    Update ggraph.vb

--- src/ggraph.vb
+++ src/ggraph.vb
@@ -4,4 +4,11 @@ Imports Microsoft.VisualBasic.Scripting.MetaData
 <Package("ggraph")>
 Module ggraph
 
+    Public Function geom_edge_link()
+
+    End Function
+
+    Public Function geom_node_point()
+
+    End Function
 End Module
