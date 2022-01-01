ec2b55c5495e81a08b355bb97d5a586386a0d255
xieguigang <xie.guigang@gcmodeller.org>
2022-01-01 23:49:31 +0800

    lauouy api

--- src/Internal/graph/layout/ggforce.vb
+++ src/Internal/graph/layout/ggforce.vb
@@ -1,10 +1,15 @@
-﻿Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+﻿Imports ggplot.options
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
 
 Namespace ggraph.layout
 
-    Public MustInherit Class ggforce
+    Public MustInherit Class ggforce : Inherits ggplotOption
 
         Public MustOverride Sub createLayout(g As NetworkGraph)
+
+        Public Overrides Function Config(ggplot As ggplot) As ggplot
+            Return ggplot
+        End Function
 
     End Class
 End Namespace
--- src/ggraph.vb
+++ src/ggraph.vb
@@ -1,6 +1,7 @@
 ﻿
+Imports ggplot.ggraph
+Imports ggplot.ggraph.layout
 Imports ggplot.ggraph.render
-Imports ggplot.layers
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
@@ -15,5 +16,15 @@ Module ggraphPkg
     <ExportAPI("geom_node_point")>
     Public Function geom_node_point() As nodeRender
         Return New nodeRender
+    End Function
+
+    <ExportAPI("layout_random")>
+    Public Function layout_random() As ggforce
+        Return New layout.random
+    End Function
+
+    <ExportAPI("layout_forcedirected")>
+    Public Function layout_forcedirected() As force_directed
+        Return New force_directed
     End Function
 End Module
