ca17138ea0274934f504e9ecc78da3c7e65ef6c0
xieguigang <xie.guigang@gcmodeller.org>
2022-01-02 01:56:25 +0800

    require makes improvements on the plot styles

--- src/Internal/graph/render/nodeRender.vb
+++ src/Internal/graph/render/nodeRender.vb
@@ -16,7 +16,7 @@ Namespace ggraph.render
         Public Overrides Function Plot(stream As ggplotPipeline) As IggplotLegendElement
             Dim graph As NetworkGraph = stream.ggplot.data
             Dim radius As Func(Of Node, Single) = Function() 20
-            Dim fontsize As Func(Of Node, Single) = Function() 13
+            Dim fontsize As Func(Of Node, Single) = Function() 10
             Dim defaultColor As Color = Color.Blue
             Dim stroke As Pen = Pens.White
             Dim baseFont As Font = CSSFont.TryParse(CSSFont.Win7Normal).GDIObject(stream.g.Dpi)
