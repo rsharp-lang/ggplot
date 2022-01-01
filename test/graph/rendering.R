9503cdcfd69f4864cea710c4ecad5dc30fa153e8
xieguigang <xie.guigang@gcmodeller.org>
2022-01-01 17:09:47 +0800

    Create rendering.R

---
+++ test/graph/rendering.R
@@ -0,0 +1,13 @@
+require(igraph);
+require(ggplot);
+require(JSON);
+
+json = json_decode(readText(`${@dir}/TCACycle.json`));
+
+str(json);
+
+nodes = as.data.frame(json$nodeDataArray);
+links = as.data.frame(json$linkDataArray);
+
+print(nodes);
+print(links);
