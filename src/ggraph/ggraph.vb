f581cef45fa936658c8a2a4e2da0cdb802c40724
xieguigang <xie.guigang@gcmodeller.org>
2022-01-03 03:24:16 +0800

    Update ggraph.vb

--- src/ggraph/ggraph.vb
+++ src/ggraph/ggraph.vb
@@ -3,7 +3,6 @@ Imports ggplot.ggraph.render
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.visualize.Network.Styling
-Imports Microsoft.VisualBasic.Data.visualize.Network.Styling.CSS
 Imports Microsoft.VisualBasic.Data.visualize.Network.Styling.FillBrushes
 Imports Microsoft.VisualBasic.Data.visualize.Network.Styling.Numeric
 Imports Microsoft.VisualBasic.Imaging
@@ -17,7 +16,7 @@ Imports SMRUCC.Rsharp.Runtime.Interop
 Imports any = Microsoft.VisualBasic.Scripting
 
 ''' <summary>
-''' A grammar of graphics for relational data
+''' # A grammar of graphics for relational data
 ''' 
 ''' ggraph is an extension of ggplot2 aimed at supporting relational 
 ''' data structures such as networks, graphs, and trees. While it 
@@ -25,11 +24,35 @@ Imports any = Microsoft.VisualBasic.Scripting
 ''' its own self-contained set of geoms, facets, etc., as well as 
 ''' adding the concept of layouts to the grammar.
 ''' </summary>
+''' <remarks>
+''' ### The core concepts
+''' 
+''' ggraph builds upon three core concepts that are quite easy To 
+''' understand:
+'''
+''' + The Layout defines how nodes are placed On the plot, that Is, 
+'''   it Is a conversion Of the relational Structure into an x And 
+'''   y value For Each node In the graph. ggraph has access To all 
+'''   layout functions available In igraph And furthermore provides 
+'''   a large selection Of its own, such As hive plots, treemaps, 
+'''   And circle packing.
+''' + The Nodes are the connected entities In the relational Structure. 
+'''   These can be plotted Using the geom_node_*() family Of geoms. 
+'''   Some node geoms make more sense For certain layouts, e.g. 
+'''   geom_node_tile() For treemaps And icicle plots, While others 
+'''   are more general purpose, e.g. geom_node_point().
+''' + The Edges are the connections between the entities In the 
+'''   relational Structure. These can be visualized Using the 
+'''   geom_edge_*() family Of geoms that contain a lot Of different 
+'''   edge types For different scenarios. Sometimes the edges are implied 
+'''   by the layout (e.g. With treemaps) And need Not be plotted, 
+'''   but often some sort Of line Is warranted.
+''' </remarks>
 <Package("ggraph")>
 Module ggraphPkg
 
     <ExportAPI("geom_edge_link")>
-    Public Function geom_edge_link(Optional mapping As Object = Nothing) As edgeRender
+    Public Function geom_edge_link(<RDefaultExpression()> Optional mapping As Object = "~aes()") As edgeRender
         Return New edgeRender
     End Function
 
@@ -64,7 +87,7 @@ Module ggraphPkg
     End Function
 
     <ExportAPI("geom_node_text")>
-    Public Function geom_node_text(Optional mapping As Object = Nothing) As textRender
+    Public Function geom_node_text(<RDefaultExpression()> Optional mapping As Object = "~aes()") As textRender
         Return New textRender
     End Function
 
