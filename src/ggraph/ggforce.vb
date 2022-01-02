bb40cb22d7eb27eca673536e9aaee4af7d32c57f
xieguigang <xie.guigang@gcmodeller.org>
2022-01-02 22:23:09 +0800

    split source file module

---
+++ src/ggraph/ggforce.vb
@@ -0,0 +1,19 @@
+﻿
+Imports ggplot.ggraph
+Imports ggplot.ggraph.layout
+Imports Microsoft.VisualBasic.CommandLine.Reflection
+Imports Microsoft.VisualBasic.Scripting.MetaData
+
+<Package("ggforce")>
+Module ggforcePkg
+
+    <ExportAPI("layout_random")>
+    Public Function layout_random() As ggforce
+        Return New layout.random
+    End Function
+
+    <ExportAPI("layout_forcedirected")>
+    Public Function layout_forcedirected() As force_directed
+        Return New force_directed
+    End Function
+End Module
--- src/ggraph/ggraph.vb
+++ src/ggraph/ggraph.vb
@@ -1,7 +1,4 @@
-﻿
-Imports ggplot.ggraph
-Imports ggplot.ggraph.layout
-Imports ggplot.ggraph.render
+﻿Imports ggplot.ggraph.render
 Imports Microsoft.VisualBasic.CommandLine.Reflection
 Imports Microsoft.VisualBasic.Scripting.MetaData
 
@@ -21,15 +18,5 @@ Module ggraphPkg
     <ExportAPI("geom_node_text")>
     Public Function geom_node_text() As textRender
         Return New textRender
-    End Function
-
-    <ExportAPI("layout_random")>
-    Public Function layout_random() As ggforce
-        Return New layout.random
-    End Function
-
-    <ExportAPI("layout_forcedirected")>
-    Public Function layout_forcedirected() As force_directed
-        Return New force_directed
     End Function
 End Module
