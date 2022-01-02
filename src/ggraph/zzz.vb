2ffaffa9252594dbd0b8d1067aa1fc2442ff4039
xieguigang <xie.guigang@gcmodeller.org>
2022-01-02 22:19:38 +0800

    register ggplot rendering driver

---
+++ src/ggraph/zzz.vb
@@ -0,0 +1,9 @@
+﻿Imports ggplot.ggraph
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+
+Public Class zzz
+
+    Public Shared Sub onLoad()
+        Call ggplot.Register(GetType(NetworkGraph), Function(theme) New graphRender(theme))
+    End Sub
+End Class
