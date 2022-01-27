c76f1081a791879460ca33b24b832b0ee5d568e0
xieguigang <xie.guigang@gcmodeller.org>
2022-01-27 14:13:13 +0800

    there is a bugs about missing parameters

--- test/lineTest.R
+++ test/lineTest.R
@@ -1,10 +1,15 @@
 require(ggplot);
 
+setwd(@dir);
+
 x = seq(-5,5, by = 0.2);
 y = sin(x);
+line_data = data.frame(x, y);
 
-bitmap(file = `${@dir}/line_sin.png`, width = 2400, height = 1600) {
-    ggplot(data.frame(x = x, y =y), aes(x = "x", y = "y"), padding = "padding: 200px 500px 200px 200px;") 
+print(line_data, max.print = 13);
+
+bitmap(file = "./line_sin.png", width = 2400, height = 1600) {
+    ggplot(line_data, aes(x = "x", y = "y"), padding = "padding: 200px 500px 200px 200px;")
     + geom_line(width = 8, show.legend = TRUE, color = "Jet")
     ;
 }
