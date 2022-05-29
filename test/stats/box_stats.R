b9566a165c0e6b3b46b5b18113b3147f1f371cde
xieguigang <xie.guigang@gcmodeller.org>
2022-05-29 10:50:26 +0800

    run data plot tests

--- test/stats/box_stats.R
+++ test/stats/box_stats.R
@@ -24,3 +24,39 @@ bitmap(file = "./myeloma_box.png", size = [3600,2100]) {
 	;
 
 }
+
+
+bitmap(file = "./myeloma_bar.png", size = [3600,2100]) {
+	
+	ggplot(myeloma, aes(x = "molecular_group", y = "DEPDC1"))
+	+ geom_barplot(width = 0.65)
+	+ geom_jitter(width = 0.3)
+	+ geom_hline(yintercept = mean(myeloma$DEPDC1), linetype="dash", line.width = 6, color = "red")# Add horizontal line at base mean 
+	+ ggtitle("DEPDC1 ~ molecular_group")
+	+ ylab("DEPDC1")
+	+ xlab("")
+	+ scale_y_continuous(labels = "F0")
+	+ stat_compare_means(method = "anova", label.y = 1600) # Add global annova p-value 
+    + stat_compare_means(label = "p.signif", method = "t.test", ref.group = ".all.", hide.ns = TRUE)# Pairwise comparison against all
+	+ theme(axis.text.x = element_text(angle = 45))
+	;
+
+}
+
+
+bitmap(file = "./myeloma_violin.png", size = [3600,2100]) {
+	
+	ggplot(myeloma, aes(x = "molecular_group", y = "DEPDC1"))
+	+ geom_violin(width = 0.65)
+	+ geom_jitter(width = 0.3)
+	+ geom_hline(yintercept = mean(myeloma$DEPDC1), linetype="dash", line.width = 6, color = "red")# Add horizontal line at base mean 
+	+ ggtitle("DEPDC1 ~ molecular_group")
+	+ ylab("DEPDC1")
+	+ xlab("")
+	+ scale_y_continuous(labels = "F0")
+	+ stat_compare_means(method = "anova", label.y = 1600) # Add global annova p-value 
+    + stat_compare_means(label = "p.signif", method = "t.test", ref.group = ".all.", hide.ns = TRUE)# Pairwise comparison against all
+	+ theme(axis.text.x = element_text(angle = 45))
+	;
+
+}
