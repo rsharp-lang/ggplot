38b82ed7918226f9fff6a6a179319e18aaf4de2b
xieguigang <xie.guigang@gcmodeller.org>
2022-05-29 09:43:03 +0800

    add R demo plot

--- test/stats/box_stats.R
+++ test/stats/box_stats.R
@@ -17,7 +17,7 @@ bitmap(file = "./myeloma_box.png", size = [3600,2100]) {
 	+ ggtitle("DEPDC1 ~ molecular_group")
 	+ ylab("DEPDC1")
 	+ xlab("")
-	+ scale_y_continuous(labels = "G2")
+	+ scale_y_continuous(labels = "F0")
 	+ stat_compare_means(method = "anova", label.y = 1600) # Add global annova p-value 
     + stat_compare_means(label = "p.signif", method = "t.test", ref.group = ".all.", hide.ns = TRUE)# Pairwise comparison against all
 	+ theme(axis.text.x = element_text(angle = 45))
