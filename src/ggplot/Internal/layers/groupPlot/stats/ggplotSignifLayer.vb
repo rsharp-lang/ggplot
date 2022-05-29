9caf36b22f2a08eb54797985337e8a83e780b35b
xieguigang <xie.guigang@gcmodeller.org>
2022-05-29 23:55:27 +0800

    Update ggplotSignifLayer.vb

--- src/ggplot/Internal/layers/groupPlot/stats/ggplotSignifLayer.vb
+++ src/ggplot/Internal/layers/groupPlot/stats/ggplotSignifLayer.vb
@@ -1,5 +1,7 @@
 ﻿Imports ggplot.elements.legend
 Imports Microsoft.VisualBasic.Imaging.d3js.scale
+Imports Microsoft.VisualBasic.Linq
+Imports Microsoft.VisualBasic.Math.Statistics.Hypothesis
 Imports SMRUCC.Rsharp.Runtime.Internal.Object
 
 Namespace layers
@@ -22,7 +24,26 @@ Namespace layers
         End Function
 
         Private Iterator Function ttest(stream As ggplotPipeline) As IEnumerable(Of compare_means)
+            Dim data = getDataGroups(stream) _
+                .ToDictionary(Function(v) v.name,
+                              Function(v)
+                                  Return v.value
+                              End Function)
 
+            For Each groupKey As String In comparision.getNames
+                Dim two As String() = comparision.getValue(Of String())(groupKey, stream.ggplot.environment)
+                Dim group1 = data.TryGetValue(two(0))
+                Dim group2 = data.TryGetValue(two(1))
+                Dim test As TwoSampleResult = t.Test(group1, group2, varEqual:=True)
+
+                Yield New compare_means With {
+                    .group1 = two(0),
+                    .group2 = two(1),
+                    .padj = test.Pvalue,
+                    .pvalue = test.Pvalue,
+                    .y = ""
+                }
+            Next
         End Function
     End Class
 End Namespace
