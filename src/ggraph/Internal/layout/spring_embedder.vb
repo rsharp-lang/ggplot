15ca31af64b5b5b2241bcc3070e8d92e2ce4e0ef
xieguigang <xie.guigang@gcmodeller.org>
2022-05-31 19:29:00 +0800

    Create spring_embedder.vb

---
+++ src/ggraph/Internal/layout/spring_embedder.vb
@@ -0,0 +1,13 @@
+﻿
+Imports Microsoft.VisualBasic.Data.visualize.Network.Graph
+Imports Microsoft.VisualBasic.Data.visualize.Network.Layouts
+
+Namespace ggraph.layout
+
+    Public Class spring_embedder : Inherits ggforce
+
+        Protected Overrides Function createAlgorithm(g As NetworkGraph) As IPlanner
+
+        End Function
+    End Class
+End Namespace
