22e85b7c0637786e870d5a7fb61ce7c348538b39
xieguigang <xie.guigang@gcmodeller.org>
2022-09-04 10:43:42 +0800

    scatter pie is not a group plot?

---
+++ src/ggplot/Internal/layers/ggplotScatterpie.vb
@@ -0,0 +1,14 @@
+﻿Imports ggplot.elements.legend
+Imports Microsoft.VisualBasic.Imaging.d3js.scale
+
+Namespace layers
+
+    Public Class ggplotScatterpie : Inherits ggplotLayer
+
+        Public Property pie As String()
+
+        Public Overrides Function Plot(stream As ggplotPipeline) As IggplotLegendElement
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
