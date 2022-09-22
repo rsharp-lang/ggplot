d9bff4c224230deb9d7157712ae8ea47da1778d0
guigang xie <xie.guigang@gcmodeller.org>
2022-09-22 09:40:45 +0800

    it seems that there is no problem of color rendering on windows?

--- test/demo3D/scatter3d.R
+++ test/demo3D/scatter3d.R
@@ -7,11 +7,24 @@ bitmap(file = `${@dir}/UMAP3d.png`, size = [2400, 2000]) {
 	data = read.csv(`${@dir}/UMAP3D.csv`, row.names = 1);
 	data[, "class"] = `class_${data[, "class"]}`;
 	
+	colorset = {
+	class_5:"red",
+	class_0:"blue",
+	class_4:"white",
+	class_1:"yellow",
+	class_9:"green",
+	class_2:"skyblue",
+	class_3:"lime",
+	class_6:"orange",
+	class_7:"steelblue",
+	class_8:"brown"
+	};
+	
 	print(unique(data[, "class"]));
 	
 	# create ggplot layers and tweaks via ggplot style options
 	ggplot(data, aes(x = "X", y = "Y", z = "Z"), padding = "padding:250px 500px 100px 100px;")
-	+ geom_point(aes(color = "class"), color = "paper", shape = "triangle", size = 20)
+	+ geom_point(aes(color = "class"), color = colorset, shape = "triangle", size = 20)
 	+ view_camera(angle = [31.5,65,125], fov = 100000)
 	+ ggtitle("Scatter UMAP 3D")
 	+ theme_black()
