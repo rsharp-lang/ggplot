955d179ebb0f01ada8ea0bfe4f779a2fbb0e884c
xieguigang <xie.guigang@gcmodeller.org>
2023-02-20 11:37:49 +0800

    Update sync_multiplegit.cmd

--- .github/sync_multiplegit.cmd
+++ .github/sync_multiplegit.cmd
@@ -2,6 +2,7 @@
 
 REM git remote remove local
 REM git remote add local http://192.168.0.232:8848/xieguigang/ggplot.git
+REM git remote add local http://git.biodeep.cn/xieguigang/ggplot.git
 
 git pull gitlink HEAD
 git pull gitee HEAD
