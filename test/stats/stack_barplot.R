1a4e413e0f446b987dfe85819fc6bb0304b13555
guigang xie <xie.guigang@gcmodeller.org>
2023-05-25 10:35:35 +0800

    Update stack_barplot.R

--- test/stats/stack_barplot.R
+++ test/stats/stack_barplot.R
@@ -1,9 +1,21 @@
+imports "package_utils" from "ggplot";
+
+package_utils::attach(`${@dir}/../../test/`);
+
 require(ggplot);
 
 bezdekIris = read.csv(system.file("data/bezdekIris.csv", package = "REnv"), row.names = NULL, check.names = FALSE);
+# bezdekIris[, "class"] = make.names(bezdekIris$class, unique = TRUE);
 
 print(bezdekIris);
 
-let mydata = melt(bezdekIris, id.vars = "Name");
+let mydata = melt(bezdekIris, id.vars = "class");
 
 print(mydata);
+
+bitmap( file = `${@dir}/stack_bar.png`) {
+    ggplot(data=data,aes("variable","value",fill="class"))
+    + geom_bar(stat="identity",position="stack", color="black", width=0.7,size=0.25)
+    + scale_fill_manual(values=c("red", "blue", "green","darkgreen","black"))
+    + labs(x = "",y = "Nutrient (mg/L)")
+}
