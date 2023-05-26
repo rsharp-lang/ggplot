9b3440ecbfc783c9c2d07cebcffd3a977ab76bba
guigang xie <xie.guigang@gcmodeller.org>
2023-05-26 14:13:28 +0800

    try to make bugs fixed for the axis range error

--- src/ggplot/Internal/layers/groupPlot/geom_bar.vb
+++ src/ggplot/Internal/layers/groupPlot/geom_bar.vb
@@ -4,16 +4,41 @@ Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Legend
 Imports Microsoft.VisualBasic.Imaging.d3js.scale
 Imports SMRUCC.Rsharp.Runtime.Internal.[Object]
 Imports SMRUCC.Rsharp.Runtime.Vectorization
+Imports stdNum = System.Math
 
 Namespace layers
 
     Public Class geom_bar : Inherits ggplotGroup
 
+        ''' <summary>
+        ''' the bar height evaluation method, value could be 
+        ''' 
+        ''' 1. identity
+        ''' 2. percentage
+        ''' </summary>
+        ''' <returns></returns>
         Public Property stat As String
         Public Property position As String
 
         Public Overrides Function getYAxis(y() As Double, ggplot As ggplot) As axisMap
+            Dim groupName As String = ggplot.base.reader.color
 
+            If stat = "percentage" Then
+                Return axisMap.FromNumeric({0, 1})
+            End If
+
+            If TypeOf ggplot.data Is dataframe Then
+                Dim groupFactors As String() = CLRVector.asCharacter(DirectCast(ggplot.data, dataframe)(groupName))
+                Dim zip = y.Zip(groupFactors).GroupBy(Function(z) z.Second).ToArray
+                Dim max As Double = zip.Max(Function(a) a.Sum(Function(i) i.First))
+                Dim min As Double = zip.Min(Function(a) a.Sum(Function(i) i.First))
+
+                min = stdNum.Min(0, min)
+
+                Return axisMap.FromNumeric({min, max})
+            Else
+                Throw New NotImplementedException
+            End If
         End Function
 
         Protected Overrides Function PlotOrdinal(stream As ggplotPipeline, x As OrdinalScale) As IggplotLegendElement
