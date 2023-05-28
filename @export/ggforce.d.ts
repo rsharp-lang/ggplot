1880e21de9401b49e3bdce0ec1147e1937e8b61f
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2023-05-28 16:58:57 +0800

    update ts/js type export header files

--- @export/ggforce.d.ts
+++ @export/ggforce.d.ts
@@ -1,5 +1,7 @@
 ﻿// export R# package module type define for javascript/typescript language
 //
+//    imports "ggforce" from "ggraph";
+//
 // ref=ggplot.ggforcePkg@ggraph, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
