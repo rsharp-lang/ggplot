be29b705eb24e97e8504f084dfc65a54364aa058
xieguigang <xie.guigang@gcmodeller.org>
2023-07-19 10:44:52 +0800

    R ggplot2 heatmap

--- test/heatmap_test.R
+++ test/heatmap_test.R
@@ -1,17 +1,19 @@
-library(ggplot2);
+library(ggplot2)
+
 
 spinrates <- read.csv("../data/spinrates.csv",
                       stringsAsFactors = FALSE)
 
-bitmap( file = "./test_heatmap.bmp");
+png(file = "./test_heatmap.png")
 
-p <- ggplot(spinrates, aes(x=velocity, y=spinrate)) +
+
+p <- ggplot(spinrates, aes(x = velocity, y = spinrate)) +
   geom_tile(aes(fill = swing_miss)) +
   scale_fill_distiller(palette = "YlGnBu", direction = 1) +
   theme_light() +
   labs(title = "Likelihood of swinging and missing on a fastball",
        y = "spin rate (rpm)")
 
-plot(p);
+plot(p)
 
-dev.off();
+dev.off()
