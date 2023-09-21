c0491e089c99501a92ba65cf30c048f0aa7d5cda
xieguigang <xie.guigang@gcmodeller.org>
2023-09-21 13:11:05 +0800

    get data group

--- src/ggpubr/ggplotConfidenceEllipse.vb
+++ src/ggpubr/ggplotConfidenceEllipse.vb
@@ -4,13 +4,16 @@ Imports ggplot
 Imports ggplot.elements.legend
 Imports ggplot.layers
 Imports Microsoft.VisualBasic.Data.ChartPlots.Statistics.PCA
+Imports Microsoft.VisualBasic.Imaging.d3js.scale
 Imports Microsoft.VisualBasic.Imaging.Math2D
 
-Public Class ggplotConfidenceEllipse : Inherits ggplotLayer
+Public Class ggplotConfidenceEllipse : Inherits ggplotGroup
 
     Public Property level As Double = 0.95
 
     Public Overrides Function Plot(stream As ggplotPipeline) As IggplotLegendElement
+        Dim allGroupData = getDataGroups(stream).ToArray
+
         Dim x As Double()
         Dim y As Double()
         Dim group As New Polygon2D(x, y)
@@ -21,5 +24,15 @@ Public Class ggplotConfidenceEllipse : Inherits ggplotLayer
         Call stream.g.FillPath(fill, path)
 
         Return Nothing
+    End Function
+
+    ''' <summary>
+    ''' no used
+    ''' </summary>
+    ''' <param name="stream"></param>
+    ''' <param name="x"></param>
+    ''' <returns></returns>
+    Protected Overrides Function PlotOrdinal(stream As ggplotPipeline, x As OrdinalScale) As IggplotLegendElement
+        Throw New NotImplementedException()
     End Function
 End Class
