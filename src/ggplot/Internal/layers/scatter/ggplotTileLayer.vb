c67c2b0e27dec4e16c871008ca5dd809e4e12410
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-01-30 09:38:25 +0800

    fix of the scale bug

--- src/ggplot/Internal/layers/scatter/ggplotTileLayer.vb
+++ src/ggplot/Internal/layers/scatter/ggplotTileLayer.vb
@@ -1,10 +1,14 @@
 ﻿Imports System.Drawing
+Imports System.IO
 Imports ggplot.elements.legend
 Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
+Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Axis
 Imports Microsoft.VisualBasic.Data.ChartPlots.Graphic.Legend
 Imports Microsoft.VisualBasic.Imaging
+Imports Microsoft.VisualBasic.Imaging.Drawing2D.Colors
 Imports Microsoft.VisualBasic.Math
 Imports Microsoft.VisualBasic.Math.LinearAlgebra
+Imports Microsoft.VisualBasic.MIME.Html.CSS
 Imports SMRUCC.Rsharp.Runtime.Vectorization
 
 Namespace layers
@@ -16,11 +20,21 @@ Namespace layers
 
     Public Class ggplotTileLayer : Inherits ggplotLayer
 
+        Public Property mapLevels As Integer = 100
+
         Private Function getFillData(stream As ggplotPipeline) As Double()
             If useCustomData OrElse useCustomColorMaps Then
                 Return reader.getMapData(Of Double)(stream.ggplot.data, reader.color, stream.ggplot.environment)
             Else
                 Return stream.y
+            End If
+        End Function
+
+        Private Function getDataLabel(stream As ggplotPipeline) As String
+            If useCustomData OrElse useCustomColorMaps Then
+                Return reader.color.ToString
+            Else
+                Return stream.ggplot.base.reader.y
             End If
         End Function
 
@@ -37,23 +51,33 @@ Namespace layers
             Dim fillData As Double() = getFillData(stream)
             Dim valuerange As New DoubleRange(fillData)
             Dim ggplot = stream.ggplot
-            Dim legends As IggplotLegendElement = Nothing
-            Dim colors As String() = getColorSet(ggplot, stream.g, 100, LegendStyles.SolidLine, fillData, legends)
+            Dim colors As String() = getColorSet(ggplot, stream.g, mapLevels, LegendStyles.SolidLine, fillData, Nothing)
             Dim textures As Brush() = colors.Select(Function(c) c.GetBrush).ToArray
-            Dim indexrange As New DoubleRange(0, 99)
+            Dim indexrange As New DoubleRange(0, mapLevels - 1)
             Dim offset As Integer
             Dim rxi, ryi As Double
 
             For i As Integer = 0 To x.Length - 1
-                rxi = stream.scale.TranslateX(x(i) - offsetx)
-                ryi = stream.scale.TranslateY(y(i) - offsety)
+                rxi = stream.scale.TranslateX(x(i)) - offsetx
+                ryi = stream.scale.TranslateY(y(i)) - offsety
                 rect = New RectangleF(New PointF(rxi, ryi), tile_size)
                 offset = valuerange.ScaleMapping(fillData(i), indexrange)
                 fill = textures(offset)
                 stream.g.FillRectangle(fill, rect)
             Next
 
-            Return legends
+            Return New legendColorMapElement With {
+                .colorMapLegend = New ColorMapLegend(stream.theme.colorSet, mapLevels) With {
+                    .title = getDataLabel(stream),
+                    .tickAxisStroke = Stroke.TryParse(stream.theme.legendTickAxisStroke).GDIObject,
+                    .tickFont = CSSFont.TryParse(stream.theme.legendTickCSS).GDIObject(stream.g.Dpi),
+                    .format = stream.theme.legendTickFormat,
+                    .ticks = fillData.CreateAxisTicks,
+                    .titleFont = CSSFont.TryParse(stream.theme.legendTitleCSS).GDIObject(stream.g.Dpi)
+                },
+                .width = stream.canvas.Padding.Right * 3 / 4,
+                .height = stream.canvas.PlotRegion.Height
+            }
         End Function
     End Class
 End Namespace
