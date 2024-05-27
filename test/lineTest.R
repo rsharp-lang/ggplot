8888d6538f23a3a2c5f416b91625cdeca1f691b2
谢桂纲 <gg.xie@bionovogene.com>
2024-05-27 15:20:50 +0800

    make bugs fixed for run stack error check

--- test/lineTest.R
+++ test/lineTest.R
@@ -1,21 +1,28 @@
 require(ggplot);
 
-setwd(@dir);
-
-x = seq(-5,5, by = 0.2);
-y = sin(x);
-line_data = data.frame(x, y);
+let x = seq(-5,5, by = 0.2);
+let y = sin(x);
+let line_data = data.frame(x, y);
 
 print(line_data, max.print = 13);
 
-bitmap(file = "./line_sin.png", width = 2400, height = 1600) {
+let plot_line = function() {
     ggplot(line_data, aes(x = "x", y = "y"), padding = "padding: 200px 500px 200px 200px;")
     + geom_line(width = 8, show.legend = TRUE, color = "Jet")
     ;
 }
 
-svg(file = "./line_sin.svg", width = 2400, height = 1600) {
-    ggplot(line_data, aes(x = "x", y = "y"), padding = "padding: 200px 500px 200px 200px;")
-    + geom_line(width = 8, show.legend = TRUE, color = "Jet")
-    ;
+let do_plot = function() {
+    let w = 2400;
+    let h = 1600;
+
+    bitmap(file = file.path(@dir,  "line_sin.png"), width = w, height = h) {
+        plot_line();
+    }
+
+    svg(file = file.path (@dir,  "line_sin.svg"), width = w, height = h) {
+        plot_line();
+    }
 }
+
+do_plot();
