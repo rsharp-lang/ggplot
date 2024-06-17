67791c5025ac3da50e96c70faf0ab245fddbd86b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-17 19:10:57 +0800

    make improvements of the stroke line helper

---
+++ src/ggplot/ggplotExtensions.vb
@@ -0,0 +1,18 @@
+﻿Imports ggplot.elements
+Imports Microsoft.VisualBasic.MIME.Html.CSS
+
+Public Module ggplotExtensions
+
+    Public Function GetStroke(line As Object, Optional default$ = "stroke: width; stroke-width: 6px; stroke-dash: solid;") As Stroke
+        If TypeOf line Is lineElement Then
+            Return DirectCast(line, lineElement).GetStroke
+        ElseIf TypeOf line Is Stroke Then
+            Return line
+        Else
+            Dim css As String = InteropArgumentHelper.getStrokePenCSS(line, "stroke: width; stroke-width: 6px; stroke-dash: solid;")
+            Dim pen As Stroke = Stroke.TryParse(css)
+
+            Return pen
+        End If
+    End Function
+End Module
