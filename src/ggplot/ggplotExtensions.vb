40fa2cdb789c5e1b81a8ada697b25be991ce748b
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-06-17 19:44:43 +0800

    configs for run outline render test

--- src/ggplot/ggplotExtensions.vb
+++ src/ggplot/ggplotExtensions.vb
@@ -3,13 +3,13 @@ Imports Microsoft.VisualBasic.MIME.Html.CSS
 
 Public Module ggplotExtensions
 
-    Public Function GetStroke(line As Object, Optional default$ = "stroke: width; stroke-width: 6px; stroke-dash: solid;") As Stroke
+    Public Function GetStroke(line As Object, Optional default$ = "stroke: white; stroke-width: 6px; stroke-dash: solid;") As Stroke
         If TypeOf line Is lineElement Then
             Return DirectCast(line, lineElement).GetStroke
         ElseIf TypeOf line Is Stroke Then
             Return line
         Else
-            Dim css As String = InteropArgumentHelper.getStrokePenCSS(line, "stroke: width; stroke-width: 6px; stroke-dash: solid;")
+            Dim css As String = InteropArgumentHelper.getStrokePenCSS(line, "stroke: white; stroke-width: 6px; stroke-dash: solid;")
             Dim pen As Stroke = Stroke.TryParse(css)
 
             Return pen
