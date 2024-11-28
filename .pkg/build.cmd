b8cf2e2430a24dff450e5098c799759aa7cd8333
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-28 09:46:26 +0800

    configs for run package msbuild

--- .pkg/build.cmd
+++ .pkg/build.cmd
@@ -1,4 +1,4 @@
-REM @echo off
+@echo off
 
 SET drive=%~d0
 SET R_HOME=%drive%/GCModeller\src\R-sharp\App\net8.0
@@ -8,4 +8,4 @@ SET js_url="https://rdocumentation.rsharp.net/assets/R_syntax.js"
 %R_HOME%/Rscript.exe --build /src ../ /save %pkg% --skip-src-build  --github-page %js_url%
 %R_HOME%/R#.exe --install.packages %pkg%
 
-pause
+REM pause
