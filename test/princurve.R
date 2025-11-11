18b7b2b63f80c0b14c914848dd343db5ba8dfd93
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-11-12 04:53:34 +0800

    plot curve for scatter

--- test/princurve.R
+++ test/princurve.R
@@ -1,21 +1,15 @@
 require(ggplot);
 
-let n <- 200;
-let t <- seq(0, 4 * PI, length.out = n);
+data(UMAP3D);
 
-str(t);
-str(rnorm(n, sd = 0.1));
-str(cos(t));
-str(sin(t));
+UMAP3D[,"class"] = `number_${UMAP3D$class}`;
 
-let x <- (t + 1) * cos(t) + rnorm(n, sd = 0.1);
-let y <- (t + 1) * sin(t) + rnorm(n, sd = 0.1);
-let data <- data.frame(x = x, y = y);
+print(UMAP3D, max.print = 20);
 
-bitmap(file = relative_work("pc_test.png")) {
-    ggplot(data, aes(x = "x", y = "y")) +
-        geom_point(alpha = 0.6, color = "steelblue", size = 12) +
-        geom_princurve(color = "red", size = 1) +
+bitmap(file = relative_work("pc_test.png"), size = []) {
+    ggplot(UMAP3D, aes(x = "X", y = "Y",color = "class")) +
+        geom_point(aes(color = "class"),alpha = 0.6, color = "paper", size = 24) +
+        geom_princurve(color = "black", size = 1) +
         labs(title = "Scatter Plot with Principal Curve",
             x = "X", y = "Y")
 }
